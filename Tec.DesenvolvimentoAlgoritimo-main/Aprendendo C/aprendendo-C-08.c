3ec32b477be16bc78baadb9454d027404c2627fa
Pedro Ribeiro <steampedroou@gmail.com>
2024-09-10 21:34:27 -0300

    Create aprendendo-C-08.c

---
+++ Tec.DesenvolvimentoAlgoritimo-main/Aprendendo C/aprendendo-C-08.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ({
+    
+int x=0; 
+int Y=0;
+
+printf ("Escreva o valor de X");
+scanf ("%i", &x);
+
+printf ("Escreva o valor de Y");
+scanf ("%i", &Y);
+
+if (x<y){
+printf (“%i" “%i”, x, Y);
+
+}else{
+
+printf ("%i" “%i”, Y, x);
+}
+
+return 0;
+}
