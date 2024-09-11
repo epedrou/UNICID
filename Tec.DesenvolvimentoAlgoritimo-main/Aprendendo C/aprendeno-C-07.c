7d6030cb211527db03d1ba2bd6631835c099c57f
Pedro Ribeiro <steampedroou@gmail.com>
2024-09-10 21:19:06 -0300

    Create aprendeno-C-07.c

---
+++ Tec.DesenvolvimentoAlgoritimo-main/Aprendendo C/aprendeno-C-07.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ({
+    
+int x=0; 
+int Y=0;
+
+printf ("Escreva o valor de X");
+scanf ("%i", &x);
+
+printf ("Escreva o valor de Y");
+scanf ("%i", &Y);
+
+if (x<y){
+printf ("X é o maior número que é igual = %i", x);
+
+}else{
+
+printf ("Y é o maior número que é igual =%i", Y);
+}
+return 0;
+}
