050d7daa418d025a866c3149eb72acbcbf00f112
Pedro Ribeiro <steampedroou@gmail.com>
2024-09-10 21:15:18 -0300

    Create aprendendo-C-06.c

---
+++ Tec.DesenvolvimentoAlgoritimo-main/Aprendendo C/aprendendo-C-06.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){ 
+
+int x=0;
+int Y=0;
+
+printf (“Escreva o valor de X”);
+scanf (“%i”, &x);
+
+printf (“Escreva o valor de Y”);
+scanf (“%i”, &Y);
+
+printf (“A soma dos dois valores é= %i”, x+Y);
+
+return 0;
+
+}
